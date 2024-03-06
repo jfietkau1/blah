c53479463d7bcbe4398a4b3617c5f24dc0f9c039
jfietkau1 <120548552+jfietkau1@users.noreply.github.com>
2024-03-06 14:35:08 -0700

    finished it I think

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import teamData from "./CollegeBasketballTeams.json";
 
 function App() {
-
-
-  const blah = "hi";
-
-
   return (
     <div className="App">
       <header className="App-header">
+        {welcomebanner()}
+        {introduction()}
+        <TeamList></TeamList>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -28,4 +27,29 @@ function App() {
   );
 }
 
+function welcomebanner() {
+  return <h1>Welcome to March Madness!!</h1>;
+}
+function introduction() {
+  return (
+    <h3>
+      This Website shows some information about all teams participating in March
+      madness
+    </h3>
+  );
+}
+function TeamList() {
+  return (
+    <div>
+      <h1>College Basketball Teams</h1>
+      <ul>
+        {teamData.teams.map((team) => (
+          <li key={team.tid}>
+            {team.school} {team.name} - {team.city}, {team.state}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 export default App;
